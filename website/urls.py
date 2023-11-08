8b03ca56406eb62398097eaf18648931c50a8450
sarath <asarath.94@gmail.com>
2023-11-08 11:36:29 +0530

    signup and login creation

---
+++ website/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home' ),
+    path('logout/', views.logout_user, name='logout' ),
+    path('signup/', views.signup_user, name='signup' ),
+]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def home(request): 
+    #check to see if someone is logging in
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        #authenticate credentials
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been Logged-in")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error logging in, Wrong credentials")
+            return redirect('home')
+
+    else:
+        return render(request, 'home.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out successfully")
+    return redirect('home')
+
+def signup_user(request):
+    return render(request, 'signup.html', {})
